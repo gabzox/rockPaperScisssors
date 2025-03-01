323a980d924c4a38b3b3e26e749baf827bce37ee
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-01 11:28:10 -0500

    added getComputerChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+function getComputerChoice()
+{
+    let randomNum = Math.floor(Math.random()*3)
+    let computerChoice;
+    switch(randomNum)
+    {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
